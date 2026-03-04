83137ac12d5fcf9390c78b4bf59e5114b8659807
alexmpa <alexander.paukonen@gmail.com>
2026-03-04 17:30:37 -0300

    Update packages

--- src/lib/build-info.ts
+++ src/lib/build-info.ts
@@ -1,4 +1,4 @@
 // AUTO-GENERATED FILE. DO NOT EDIT.
 // This file is overwritten by scripts/generate-build-info.mjs on every build.
-export const BUILD_TIME = "2026-03-04T04:53:01.487Z";
-export const BUILD_GIT_SHA = "7f2f79c";
+export const BUILD_TIME = "2026-03-04T19:10:50.687Z";
+export const BUILD_GIT_SHA = "f33e802";
